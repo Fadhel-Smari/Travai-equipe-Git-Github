e44a8ab9b57a8872c7bf5ee6334b153a30b51788
Gabriel Cadieux <gabrielcadieux96@gmail.com>
2025-12-10 15:41:08 -0500

    Création de la branche Gabriel_Cadieux et fct pour afficher entrées

--- Exercice 1/menu.py
+++ Exercice 1/menu.py
@@ -3,5 +3,14 @@ def main():
     print("=== MENU DU RESTAURANT ===")
     # Les autres ajouteront leur code ici
 
+    #ajouts des entrées
+    def afficher_entrees():
+        liste_entrees = ["soupe aux légumes", "salade césar", "6 aile de poulet"]
+        print("\n=ENTRÉES=")
+        for entree in liste_entrees:
+            print(entree)
+    afficher_entrees()
+
+
 if __name__ == "__main__":
     main()

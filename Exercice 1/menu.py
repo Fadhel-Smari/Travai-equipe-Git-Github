c92ab353917f2e369e2dd0f2da50750b58fa2297
Bhatti Unaiza Ali <unaizaab@gmail.com>
2025-12-12 09:48:49 -0500

    j'ai  fixer main  et ajouetrafficher_desserts

--- Exercice 1/menu.py
+++ Exercice 1/menu.py
@@ -3,14 +3,11 @@ def main():
     print("=== MENU DU RESTAURANT ===")
     # Les autres ajouteront leur code ici
 
-if __name__ == "__main__":
-    main()
-
-
 def afficher_desserts():
     desserts = ["Mochi Ice Cream", "Kheer", "Tiramisu"]
     for d in desserts:
         print(d)
 
-def main():
-    afficher_desserts()
+
+if __name__ == "__main__":
+    main()

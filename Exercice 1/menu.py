3ea07023566b2ff9ca183d52c03c25907cd08354
Bhatti Unaiza Ali <unaizaab@gmail.com>
2025-12-15 14:47:20 -0500

    Ajouter la fonctiom affichage_desserts.

--- Exercice 1/menu.py
+++ Exercice 1/menu.py
@@ -1,13 +1,13 @@
-# Structure de départ
-def main():
-    print("=== MENU DU RESTAURANT ===")
-    # Les autres ajouteront leur code ici
 
 def afficher_desserts():
     desserts = ["Mochi Ice Cream", "Kheer", "Tiramisu"]
+    print("=== DESSERTS ===")
     for d in desserts:
         print(d)
 
+def maim():
+    print("=== MENU DU RESTAURANT ===")
+    afficher_desserts()
 
 if __name__ == "__main__":
     main()

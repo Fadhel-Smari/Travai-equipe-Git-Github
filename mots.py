fa89f736f40f9812f26774fcb7e0d6144337b895
usernzl <unaizaab@gmail.com>
2025-12-16 14:43:10 -0500

    Delete mots.py

--- mots.py
+++
@@ -1,7 +0,0 @@
-import random
-
-# Fonction pour retourne un mot aléatoire de la liste
-def choisir_mot():
-    mots = ["python", "programmation", "github", "collaboration"]
-    mot_aleatoire = random.choice(mots)
-    return mot_aleatoire

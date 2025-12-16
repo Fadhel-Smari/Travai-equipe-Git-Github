ea00e8f71e8169366170a185300a0e9b8944375f
Gabriel Cadieux <gabrielcadieux96@gmail.com>
2025-12-15 19:33:48 -0500

    Ajout de la fonction pour l'affichage du jeux et la progression

--- Exercice 3/pendu/affichage.py
+++ Exercice 3/pendu/affichage.py
@@ -1,3 +1,81 @@
+vie_restant = 0
+#Intro au jeux
+def affichage_depart():
+    print("=== Jeu du Pendu ===")
+
+#affiche different stage de pendu, nb vie et game over
 def afficher_pendu(erreurs):
-    pass
-    # Dessins ASCII selon le nombre d'erreurs
+    print(f"{vie_restant}/6 vie")
+    stage = [
+        '''
+  ¤---¬
+  |   |
+  Ø   |
+ /|\\  |
+ / \\  |
+      |
+=========
+        ''',
+        '''
+  ¤---¬
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+=========
+        ''',
+        '''
+  ¤---¬
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========
+        ''',
+        '''
+  ¤---¬
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+        ''',
+        '''
+  ¤---¬
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+        ''',
+        '''
+  ¤---¬
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+        ''',
+        '''
+  ¤---¬
+  |   |
+      |
+      |
+      |
+      |
+=========
+        ''',]
+    print(stage[erreurs])
+    if vie_restant == 0:
+        print("=== Vous avez perdu ===")
+
+
+#affiche le nom du jeux
+affichage_depart()
+#affiche la progression du jeux
+afficher_pendu(vie_restant)

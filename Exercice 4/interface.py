bc8348d0e80a24274b294420efb81eb44ff0746e
Gabriel Cadieux <gabrielcadieux96@gmail.com>
2025-12-16 18:42:00 -0500

    feat: Ajout def demander_infos et afficher_resultat

--- Exercice 4/interface.py
+++ Exercice 4/interface.py
@@ -1,7 +1,20 @@
 def demander_infos():
-    pass
     # Demande poids et taille
+    poids = float(input("Veuillez indiquez votre poids(kg): "))
+    taille = float(input("Veuillez indiquez votre taille(): "))
+    return(poids,taille)
+
 
 def afficher_resultat(imc, categorie):
-    pass
     # Affiche joliment
+    print(f"Vous avez un IMC de: {imc:.2f}")
+    print(f"Votre categorie est: {categorie}")
+
+
+import calculs
+import interface
+
+poids, taille = interface.demander_infos()
+imc = calculs.calculer_imc(poids,taille)
+categorie = calculs.interpreter_imc(imc)
+interface.afficher_resultat(imc, categorie)

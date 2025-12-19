ac3250bc0aed20250143d7ad6a8e9ad6221427b9
Gabriel Cadieux <gabrielcadieux96@gmail.com>
2025-12-19 14:04:21 -0500

    feat: Ajout de changer_statut pour cohérence avec ticket et retrait des autres fonction inutile

--- Exercice 5/tickets/gestion.py
+++ Exercice 5/tickets/gestion.py
@@ -1,16 +1,18 @@
-import sauvegarde
-tickets = []
+import ticket
 
-def ajouter_ticket():
-        nouveau_ticket = input("Ticket a ajouter: ")
-        tickets.append(nouveau_ticket)
+def changer_statut():
+        titre = input("Titre du ticket a modifier: ")
+
+        if titre in tickets:
+                print("1. À faire\n2. En Cours\n3. Terminer")
+                choix = input("Nouveau statu(1-3): ")
+
+                if choix == "1":
+                        ticket[titre].statut = "À faire"
+                elif choix == "2":
+                        ticket[titre].statut = "En Cours"
+                elif choix == "3":
+                        ticket[titre].statut = "Terminer"
+
+                print(f"Statut de {titre} changé à: {ticket[titre].statut}")
         return tickets
-
-def supprimer_ticket():
-        ticket_supprimer = input("Ticket a enlevé:")
-        tickets.remove(ticket_supprimer)
-        return tickets
-
-
-
-

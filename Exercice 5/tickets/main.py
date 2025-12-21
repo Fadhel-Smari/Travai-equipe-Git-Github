6ab88b7cfc492d16a12847d5cc133c8fdc341c52
Fadhel-Smari <smarifadhel@gmail.com>
2025-12-21 17:51:21 -0500

    feat/exercice5: création d’un conflit sur main.py

--- Exercice 5/tickets/main.py
+++ Exercice 5/tickets/main.py
@@ -1,12 +1,18 @@
 import gestion
 import ticket
+import sauvegarde
+
 
 def main():
-
-    tickets = {}
+    tickets = {t.id_ticket: t for t in sauvegarde.charger_tickets()}
 
     while True:
-        requete = input("1 - ajout de ticket\n2 - changer statut\n3 - quitter\n ")
+        requete = input(
+            "1 - Ajouter un ticket\n"
+            "2 - Modifier le statut d’un ticket\n"
+            "3 - Quitter l’application\n"
+            "Choix : "
+        )
 
         match requete:
             case "1":
@@ -14,8 +20,15 @@ def main():
             case "2":
                 tickets = gestion.changer_statut(tickets)
             case "3":
+                sauvegarde.sauvegarder_tickets(tickets.values())
+                print("Application fermée.")
                 break
-    
+            case _:
+                print("Choix invalide, veuillez réessayer.")
+
+    print("\nListe des tickets :")
     for t in tickets.values():
-        print(f"{t.id_ticket} -  {t.titre} - {t.description} - {t.statut}")
-main() 
+        print(f"[{t.id_ticket}] | {t.titre} | {t.statut}")
+
+
+main()

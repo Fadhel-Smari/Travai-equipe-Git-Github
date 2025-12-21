5dfd45d6e44f5a661d76a0b22c0af17406436ec5
Fabiola.s. <favry0001@gmail.com>
2025-12-21 14:37:15 -0500

    feature : Finalisation de l'historique

--- Exercice 4/historique.py
+++ Exercice 4/historique.py
@@ -1,7 +1,25 @@
 def sauvegarder_calcul(nom, imc):
-    pass
+    
     # Sauvegarde dans un fichier
+    nom_final = nom if nom else "Anonyme"
+    with open(FICHIER_HISTORIQUE, "a", encoding="utf-8") as f:
+        f.write(f"{nom_final};{imc:.2f}\n")
 
 def afficher_historique():
-    pass
+
     # Lit le fichier
+    try:
+        with open(FICHIER_HISTORIQUE, "r", encoding="utf-8") as f:
+            lignes = f.readlines()
+            
+        if not lignes:
+            print("L'historique est vide.")
+            return
+
+        print("\n=== Historique des IMC ===")
+        for i, ligne in enumerate(lignes, start=1):
+            if ";" in ligne:
+                nom, val = ligne.strip().split(";")
+                print(f"{i}. {nom} -> IMC: {val}")
+    except FileNotFoundError:
+        print("Aucun fichier d'historique trouvé.")
